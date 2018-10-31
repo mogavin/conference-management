94dcbd43085205265c6d630f176c60de26275a8a
mogavin <andre.gavino@certsys.com.br>
2018-10-31 01:41:09 -0300

    teste husky

--- src/models/session.js
+++ src/models/session.js
@@ -3,6 +3,7 @@ const { getUtcDate, getMinutesDiff, addMinutes } = require("../util/date");
 const session = ({ name, start, end, talks = [], availableTime = start } = {}) => {
   const minutesRemaining = getMinutesDiff(availableTime, end),
     hasTime = minutesRemaining > 0;
+
   return {
     addTalk: ({ name, length }) => {
       const required = {
